409dc1e1a8fc7c292ed7e3f184cd1281c2f0c308
Sjon Kosse <sjon@x61.eu>
2015-12-21 17:53:14 +0100

    I should make this like sort.Sort
    Turns out generating permutations of 20! isn't all that useful.

--- utils.go
+++ utils.go
@@ -1,6 +1,7 @@
 package advent
 
-func Permutations(s []string) [][]string {
+//PermutationsString generates all permutations of a string slice
+func PermutationsString(s []string) [][]string {
 	var output [][]string
 
 	var heapPermute func(s []string, n int)
@@ -8,6 +9,32 @@ func Permutations(s []string) [][]string {
 	heapPermute = func(s []string, n int) {
 		if n == 1 {
 			tmp := make([]string, len(s))
+			copy(tmp, s)
+			output = append(output, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				heapPermute(s, n-1)
+				if n%2 == 1 {
+					s[0], s[n-1] = s[n-1], s[0]
+				} else {
+					s[i], s[n-1] = s[n-1], s[i]
+				}
+			}
+		}
+	}
+	heapPermute(s, len(s))
+	return output
+}
+
+//PermutationsInt generates all permutations of a int slice
+func PermutationsInt(s []int) [][]int {
+	var output [][]int
+
+	var heapPermute func(s []int, n int)
+
+	heapPermute = func(s []int, n int) {
+		if n == 1 {
+			tmp := make([]int, len(s))
 			copy(tmp, s)
 			output = append(output, tmp)
 		} else {
--- utils_test.go
+++ utils_test.go
@@ -2,6 +2,10 @@ package advent
 
 import "testing"
 
-func TestPermutations(t *testing.T) {
-	t.Log(Permutations([]string{"ayy", "may", "bay"}))
+func TestPermutationsString(t *testing.T) {
+	t.Log(PermutationsString([]string{"ayy", "may", "bay"}))
+}
+
+func TestPermutationsInt(t *testing.T) {
+	t.Log(PermutationsInt([]int{1, 2, 3, 4, 5}))
 }
